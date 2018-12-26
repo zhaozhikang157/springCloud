0e46e506733eff60db0ad2fbae1ac7355c53dcc5
zhaozhikang <1063698024@qq.com>
2018-12-26 11:40:38 +0800

    分布式配置中心集群

--- cloudConfigClient/src/main/java/com/cloud/application/ConfigClientApplication.java
+++ cloudConfigClient/src/main/java/com/cloud/application/ConfigClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.cloud"})
 @RestController
+@EnableEurekaClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
